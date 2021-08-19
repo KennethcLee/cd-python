16f5b55070621198c8f21c507eab7861a2ebb569
klee@blockrules <klee@l-brmac06.local>
2021-08-19 07:29:08 -0700

    Counter

--- week3/Counter/server.py
+++ week3/Counter/server.py
@@ -7,13 +7,30 @@ app.secret_key="Test"
 def counter():
     if 'count' not in session:
         session['count']=0
-    else:
+    elif 'anynum' not in session:
         session['count']+=1
+    if 'visit' not in session:
+        session['visit']=0
+    else:
+        session['visit']+=1
+    return render_template('/index.html')
+
+@app.route('/addtwo')
+def addtwo():
+    if 'count' not in session:
+        session['count']=0
+    else:
+        session['count']+=2
     return render_template('/index.html')
 
 @app.route('/reset')
 def reset():
-    session['count']=-1
+    session.pop('count')
+    return redirect('/')
+
+@app.route('/anynum', methods=['POST'])
+def anynum():
+    session['count']+=(int(request.form['anynum']))
     return redirect('/')
 
 if __name__=="__main__":
