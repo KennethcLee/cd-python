e5a7eaff22a9096989fd9b2f0913e05918f56044
kennethclee@gmail.com <kennethclee@gmail.com>
2021-08-31 22:43:11 -0700

    MVC

---
+++ week5/flask_app/controllers/users.py
@@ -0,0 +1,70 @@
+from flask import redirect, render_template, request, session, url_for
+from flask_app import app
+from flask_app.models.user import Users
+
+@app.route('/', methods=['GET'])
+def index():
+    users = Users.get_all()
+    print('***  1  ***', users)
+    for j in users:
+        print('***  1A  ***', j.updated_at)
+    return render_template("/index.html", all_users = users)
+
+@app.route('/adduser', methods=['POST'])
+def adduser():
+    print('***  3  ***')
+    return render_template("/adduser.html")
+
+@app.route('/showuser/<int:id>')
+def showuser(id):
+    print('***  6  ***')
+    data = {
+        'id': id
+    }
+    user = Users.get_user(data)
+    print('***  6A  ***', user[0]['id'])
+    return render_template("/showuser.html", user=user)
+
+@app.route('/edituser/<int:id>')
+def edituser(id):
+    print('***  5  ***')
+    data = {
+        'id': id,
+    }
+    user = Users.get_user(data)
+    return render_template("/edituser.html", user=user)
+
+@app.route('/save', methods=['POST'])
+def save():
+    print('***  7  ***')
+    data = {
+        'fname': request.form['fname'],
+        'lname': request.form['lname'],
+        'email': request.form['email']
+    }
+    print('***  7A  ***')
+    Users.save(data)
+    return redirect("/")
+
+@app.route('/edit/<int:id>', methods=['POST'])
+def edit(id):
+    print('***  8  ***')
+    data = {
+        'id': id,
+        'fname': request.form['fname'],
+        'lname': request.form['lname'],
+        'email': request.form['email']
+    }
+    user=Users.edit(data)
+    print('***  8A  ***', data)
+    return redirect(url_for('edituser', id=id))
+
+@app.route('/deleteuser/<int:id>')
+def delete(id):
+    print('***  9  ***')
+    data = {
+        'id': id,
+    }
+    Users.delete(data)
+    print('***  9A  ***', data)
+    return redirect(url_for('index'))
---
+++ week5/flask_app/models/user.py
@@ -0,0 +1,50 @@
+from werkzeug.wrappers import request
+from flask_app.config.mysqlconnection import connectToMySQL
+
+class Users:
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+    @classmethod
+    def get_user(cls, data):
+        print('***  11  ***', data)
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        print('***  11A  ***', query)
+        results = connectToMySQL('user_ca').query_db(query, data)
+        print('***  11B  ***', results)
+        return results
+    @classmethod
+    def get_all(cls):
+        print('***  13  ***')
+        query = "SELECT * FROM users;"
+        results = connectToMySQL('user_ca').query_db(query)
+        users = []
+        print('***  13A  ***', users)
+        for j in results:
+            users.append( cls(j) )
+        print('***  13B  ***', users[0].last_name)
+        return users
+
+    @classmethod
+    def save(cls, data):
+        print('***  15  ***', data)
+        query = "INSERT INTO users (first_name, last_name, email, created_at, updated_at) VALUES ( %(fname)s, %(lname)s, %(email)s, now(), now());"
+        return connectToMySQL('user_ca').query_db(query, data)
+
+    @classmethod
+    def edit(cls, data):
+        print('***  17  ***', data)
+        query = "UPDATE users SET first_name = %(fname)s, last_name = %(lname)s, email = %(email)s, updated_at = now() WHERE id = %(id)s;"
+        print('***  17A  ***', query)
+        return connectToMySQL('user_ca').query_db(query, data)
+
+    @classmethod
+    def delete(cls, data):
+        print('***  19  ***', data)
+        query = "DELETE FROM users WHERE id = %(id)s;"
+        print('***  19A  ***', query)
+        return connectToMySQL('user_ca').query_db(query, data)
